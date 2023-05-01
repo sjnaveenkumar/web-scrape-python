9f14b7d9d8ebbf71f9a47da4af8542c75dc8f426
Naveen Kumar <sjnaveenkumar@gmail.com>
2023-05-02 03:20:19 +0530

    enum issues fixed

--- search.py
+++ search.py
@@ -2,5 +2,5 @@ from scrape_service import browse_and_extract
 from enums import Template
 
 def do_search(query,page=1):
-    return browse_and_extract("https://www.amazon.in/s?k="+query+"&page="+page,enums.Template.SEARCH)
+    return browse_and_extract("https://www.amazon.in/s?k="+query+"&page="+page,Template.SEARCH)
     
