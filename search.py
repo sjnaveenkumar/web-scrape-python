36e682df82ecf25e4f7fa2247268074862e0fd2f
Naveen Kumar <sjnaveenkumar@gmail.com>
2023-05-01 16:51:08 +0530

    added requirement txt and updated readme

--- search.py
+++ search.py
@@ -1,5 +1,5 @@
 from scrape_service import *
 
-def do_search(query,page=0):
+def do_search(query,page=1):
     return browse_and_extract("https://www.amazon.in/s?k="+query+"&page="+page,'search')
     
